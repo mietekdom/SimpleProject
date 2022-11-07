947cd0c5d44fb18deadd3d10e832f96edc17b889
miecz <miecz@LAPTOP-UVM3H6HN>
2022-11-07 14:13:48 +0100

    create new file

---
+++ SimpleProject01/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleProject01
+{
+    class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        private DateTime dateOfBirth;
+
+        public Person(string firstName, string lastName)
+        {
+            Console.WriteLine("Constructor 1");
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person(DateTime dateOfBirth, string firstName, string lastName) : this(firstName, lastName)
+        {
+            Console.WriteLine("Constructor 2");
+            SetDateOfBirth(dateOfBirth);
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void SetDateOfBirth(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date");
+            }
+            else
+            {
+                dateOfBirth = date;
+            }
+        }
+
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, Im {FirstName}, {GetDateOfBirth()}");
+        }
+    }
+}
--- SimpleProject01/Program.cs
+++ SimpleProject01/Program.cs
@@ -6,7 +6,16 @@ namespace SimpleProject01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person bill = new Person("Bill", "Wick");
+            //Person john = new Person(new DateTime(1990, 8, 2), "John", "Wick");
+
+            bill.SetDateOfBirth(new DateTime(1990, 1, 2));
+
+
+            bill.SayHi();
+
+            Person john = new Person(new DateTime(1990, 8, 2), "John", "Wick");
+            john.SayHi();
         }
     }
 }
